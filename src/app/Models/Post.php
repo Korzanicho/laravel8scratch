26cb564cce7f1eebab4f9dcd4b7fa6f132207c46
Adrian Korzan <adrian.korzan@telestrada.pl>
2021-12-06 15:56:52 +0100

    (feature): eloquent

--- src/app/Models/Post.php
+++ src/app/Models/Post.php
@@ -2,71 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use phpDocumentor\Reflection\Types\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public $title;
+    protected $guarded = [];
+//    protected $fillable = ['title', 'excerpt', 'body', 'category_id', 'slug'];
 
-    public $slug;
-
-    public $excerpt;
-
-    public $date;
-
-    public $body;
-
-    /**
-     * Post constructor.
-     * @param $title
-     * @param $excerpt
-     * @param $date
-     * @param $body
-     */
-    public function __construct($title, $slug, $excerpt, $date, $body)
+    public function category()
     {
-        $this->title = $title;
-        $this->slug = $slug;
-        $this->excerpt = $excerpt;
-        $this->date = $date;
-        $this->body = $body;
+        // hasOne, hasMany, belongsTo, belongsToMany
+        return $this->belongsTo(Category::class);
     }
 
-    public static function find($slug)
-    {
-        return static::all()->firstWhere('slug', $slug);
-    }
-
-    public static function findOrFail($slug)
-    {
-        $post = static::find('slug', $slug);
-
-        if (! $post) {
-            throw new ModelNotFoundException();
-        }
-
-        return $post;
-    }
-
-    public static function all(): \Illuminate\Support\Collection {
-        return cache()->rememberForever('posts.all', function() {
-            return collect(\Illuminate\Support\Facades\File::files(resource_path("posts")))
-                ->map(function ($file) {
-                    return \Spatie\YamlFrontMatter\YamlFrontMatter::parseFile($file);
-                })
-                ->map(function ($document) {
-
-                    return new \App\Models\Post(
-                        $document->title,
-                        $document->slug,
-                        $document->excerpt,
-                        $document->date,
-                        $document->body()
-                    );
-                })
-                ->sortByDesc('date');
-        });
-    }
+    use HasFactory;
 }
