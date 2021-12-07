05b89e5978cf4ed514e6ac28bdcfc64ffe841f37
Adrian <adrian.korzan@gmail.com>
2021-12-07 22:13:46 +0100

    (feature): factory

--- src/app/Models/Post.php
+++ src/app/Models/Post.php
@@ -10,15 +10,17 @@ class Post extends Model
     protected $guarded = [];
 //    protected $fillable = ['title', 'excerpt', 'body', 'category_id', 'slug'];
 
+    protected $with = ['category', 'author'];
+
     public function category()
     {
         // hasOne, hasMany, belongsTo, belongsToMany
         return $this->belongsTo(Category::class);
     }
 
-    public function user()
+    public function author()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     use HasFactory;
--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use \App\Models\Post;
+use \App\Models\User;
 use \App\Models\Category;
 
 /*
@@ -17,7 +18,7 @@ use \App\Models\Category;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::with('category')->get()
+        'posts' => Post::latest('published_at')->with(['category', 'author'])->get()
     ]);
 });
 
@@ -32,7 +33,13 @@ Route::get('posts/{post:slug}', function (Post $post) {
 Route::get('categories/{category:slug}', function (Category $category) {
 
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts->load(['category', 'author'])
     ]);
+});
 
+Route::get('authors/{author:username}', function (User $author) {
+
+    return view('posts', [
+        'posts' => $author->posts->load(['category', 'author'])
+    ]);
 });
